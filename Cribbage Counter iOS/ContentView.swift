eb70b8555aa158844489015a0b8b69dc7bbc0b9b
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-10 16:38:22 -0700

    created application and started with some initial ui elements and functionality

--- Cribbage Counter iOS/ContentView.swift
+++ Cribbage Counter iOS/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Players: Int = 0;
+    @State var scores: [String: Int] = [:];
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Pick the number of players")
+            Button("2", action: two_players);
+            Button("3", action: three_players)
+            Text("\(Players)")
+            List {
+                ForEach(scores.sorted(by: >), id: \.key) { key, value in
+                    Text("\(value) \(key)")
+                }
+            }
+        
+            
         }
         .padding()
+    }
+    
+    func two_players() {
+        Players = 2
+        scores["Player1"] = 0;
+        scores["Player2"] = 0;
+    }
+    func three_players() {
+        two_players()
+        scores["Player3"] = 0;
+        Players += 1;
     }
 }
 
