80f69170e4abebdeace543838a1dab8746c51e11
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-10 15:52:48 -0700

    Initial Commit

---
+++ Cribbage Counter iOS/Cribbage_Counter_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cribbage_Counter_iOSApp.swift
+//  Cribbage Counter iOS
+//
+//  Created by Husayn Esmail on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct Cribbage_Counter_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
